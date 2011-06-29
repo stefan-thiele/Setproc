e2282cfbddbb78fa16c2a986345d0ae44f475e91
Romain Vincent <romain.vincent.40@gmail.com>
2011-06-29 15:48:41 +0200

    Save after critical crash

---
+++ operator.py
@@ -0,0 +1,11 @@
+"""
+First try of seconde quantification
+the system is with one level spin down.
+The basis is
+|0> |1>
+"""
+
+#Operator creation
+cp = matrix([[0,0],[1,0]])
+cm = matrix([[0,1],[0,0]])
+
