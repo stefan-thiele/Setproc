3458c08578610907e1c961fb740659b6b9416460
Romain Vincent <romain.vincent.40@gmail.com>
2011-07-11 13:06:40 +0200

    File cleaning before documentation, merging of functions and improvement

--- therm_pop.py
+++
@@ -1,9 +0,0 @@
-def Temp_Pop(T,E) :
-	E2 = E[1] - E[0]
-	E3 = E[2] - E[0]
-	E4 = E[3] - E[0]
-	P2 = 0.25 * exp(-E2/T)
-	P3 = 0.25 * exp(-E3/T)
-	P4 = 0.25 * exp(-E4/T)
-	P1 = 1 - P4 - P2 -P3
-	return [P1,P2,P3,P4]
