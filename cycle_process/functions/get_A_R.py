cea484e969f7ec2b7b721aca13895942e520eb68
Romain Vincent <romain.vincent.40@gmail.com>
2011-11-05 14:37:00 +0100

    get_A_T takes now full avantage of the new methods of the Stat_point class Now, when two jumps are present, the first is taken for the trace and the second for the retrace, which in time correspond to the tow consecutive events.

--- cycle_process/functions/get_A_R.py
+++ cycle_process/functions/get_A_R.py
@@ -1,65 +1,48 @@
-
-
+from numpy import size
 
 def get_A_R(Cyc) :
-"""
-This function parse the data and store all the sweep for which there was a jump both for the trace and retrace. They are store in ["AvsR"], the first element ["AvsR"][0] being the trace and the second the retrace.
-"""
-seuil1 = self["calibration"]["plot"]["seuil1"]
-seuil2 = self["calibration"]["plot"]["seuil2"]
-self["AvsR"] = [[], []]
-size_detect = size(self["detection"])
-itera = size_detect/4
-for i in range(itera-4) :
-	traceok = False
-	retraceok = False
-	trace1 = self["detection"][4*i+4] #The +4 is due to the fact that the waiting time is done during trace
-	trace2 = self["detection"][4*i+1+4]
-	retrace1 = self["detection"][4*i+2]
-	retrace2 = self["detection"][4*i+3]
-	#check first which trace has to be taken
-	if(abs(trace1.value) > seuil1 and abs(trace2.value) > seuil1) :
-	if(abs(trace1.value) < seuil2 and abs(trace2.value) < seuil2) :
-		if abs(trace1.value) > abs(trace2.value) :
-		trace_push = trace1.field
-		traceok = True
-		else :
-		trace_push = trace2.field
-		traceok = True
-	elif abs(trace1.value) < seuil2 :
-		trace_push = trace1.field
-		traceok = True
-	elif abs(trace2.value) < seuil2 :
-		trace_push = trace2.field
-		traceok = True
-	elif abs(trace1.value) > seuil1 and abs(trace1.value) < seuil2 :
-	trace_push = trace1.field
-	traceok = True
-	elif abs(trace2.value) > seuil1 and abs(trace2.value) < seuil2 :
-	trace_push = trace2.field
-	traceok = True
-	if(traceok) :
-	if(abs(retrace1.value) > seuil1 and abs(retrace2.value) > seuil1) :
-		if(abs(retrace1.value) < seuil2 and abs(retrace2.value) < seuil2) :
-		if abs(retrace1.value) > abs(retrace2.value) :
-			retrace_push = retrace1.field
-			retraceok = True
-		else :
-			retrace_push = retrace2.field
-			retraceok = True
-		elif abs(retrace1.value) < seuil2 :
-		retrace_push = retrace1.field
-		retraceok = True
-		elif abs(retrace2.value) < seuil2 :
-		retrace_push = retrace2.field
-		retraceok = True
-	elif abs(retrace1.value) > seuil1 and abs(retrace1.value) < seuil2 :
-		retrace_push = retrace1.field
-		retraceok = True
-	elif abs(retrace2.value) > seuil1 and abs(retrace2.value) < seuil2 :
-		retrace_push = retrace2.field
-		retraceok = True
+    """
+    This function parse the data and store all the sweep for which there was a jump both for the trace and retrace. They are store in ["AvsR"], the first element ["AvsR"][0] being the trace and the second the retrace.
+    """
+    seuil1 = Cyc["calibration"]["plot"]["seuil1"]
+    seuil2 = Cyc["calibration"]["plot"]["seuil2"]
+    Cyc["AvsR"] = [[], []]
+    size_detect = size(Cyc["detection"])
+    itera = size_detect/4
+    for i in range(itera-4) :
+        traceok = False
+        retraceok = False
+        trace1 = Cyc["detection"][4*i+4] #The +4 is due to the fact that the waiting time is done during trace
+        trace2 = Cyc["detection"][4*i+1+4]
+        retrace1 = Cyc["detection"][4*i+2]
+        retrace2 = Cyc["detection"][4*i+3]
+        #FOR TRACE
+        if trace1.in_between(seuil1, seuil2) and trace2.in_between(seuil1, seuil2) : #check if the two values are in the range
+            #if yes, take the higher value
+            trace_push = min(trace1.field, trace2.field) #I want the position of the FIRST jump
+            traceok = True
 
-	if(traceok and retraceok):
-	self["AvsR"][0].append(trace_push)
-	self["AvsR"][1].append(retrace_push)
+        elif trace1.in_between(seuil1, seuil2) :
+            trace_push = trace1.field
+            traceok = True
+
+        elif trace2.in_between(seuil1, seuil2) :
+            trace_push = trace2.field
+            traceok = True
+
+        if(traceok): #only do the check if a trace is valid
+            if retrace1.in_between(seuil1, seuil2) and retrace2.in_between(seuil1, seuil2) : #check if the two values are in the range
+                #if yes, take the higher value
+                retrace_push = min(retrace1.field, retrace2.field) #I want the position of the LAST jump
+                retraceok = True
+            elif retrace1.in_between(seuil1, seuil2) :
+                retrace_push = retrace1.field
+                retraceok = True
+            elif retrace2.in_between(seuil1, seuil2) :
+                retrace_push = retrace2.field
+                retraceok = True
+
+
+        if(traceok and retraceok):
+            Cyc["AvsR"][0].append(trace_push)
+            Cyc["AvsR"][1].append(retrace_push)
